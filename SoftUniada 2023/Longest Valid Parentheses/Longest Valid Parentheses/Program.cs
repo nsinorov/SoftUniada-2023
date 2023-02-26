13caa606aba00dd1f11eaa1e06509a0ee839f75f
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-26 17:47:30 +0200

    Add files via upload

---
+++ SoftUniada 2023/Longest Valid Parentheses/Longest Valid Parentheses/Program.cs
@@ -0,0 +1,31 @@
+﻿string parantheses = Console.ReadLine();
+
+int maxCount = 0;
+
+Stack<int> stack = new();
+
+stack.Push(-1);
+
+for (int i = 0; i < parantheses.Length; i++)
+{
+    if (parantheses[i] == '(')
+    {
+        stack.Push(i);
+    }
+    else
+    {
+        stack.Pop();
+
+        if (stack.Count == 0)
+        {
+            stack.Push(i);
+        }
+        else
+        {
+            int start = stack.Peek();
+            maxCount = Math.Max(maxCount, i - start);
+        }
+    }
+}
+
+Console.WriteLine(maxCount);
