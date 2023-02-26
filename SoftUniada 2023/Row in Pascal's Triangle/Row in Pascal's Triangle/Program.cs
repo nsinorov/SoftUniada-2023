f1fe04a61c3ac3a7423cc3d18d8f0a879bcdd634
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-26 17:48:43 +0200

    Add files via upload

---
+++ SoftUniada 2023/Row in Pascal's Triangle/Row in Pascal's Triangle/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int[] currentRow = new int[n + 1];
+int[] previousRow = new int[n + 1];
+
+currentRow[0] = 1;
+previousRow[0] = 1;
+
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 1; j < i; j++)
+    {
+        currentRow[j] = previousRow[j - 1] + previousRow[j];
+    }
+
+    currentRow[i] = 1;
+
+    Array.Copy(currentRow, previousRow, currentRow.Length);
+
+}
+
+Console.WriteLine(string.Join(" ", currentRow));
