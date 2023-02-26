f6f2868ca708c82b429a8b95a75e12ed344510b4
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-26 17:48:20 +0200

    Add files via upload

---
+++ SoftUniada 2023/Rotate Matrix/Rotate Matrix/Program.cs
@@ -0,0 +1,33 @@
+﻿int size = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[size, size];
+
+// Въвеждане на матрицата
+for (int i = 0; i < size; i++)
+{
+    string[] rowValues = Console.ReadLine().Split();
+    for (int j = 0; j < size; j++)
+    {
+        matrix[i, j] = int.Parse(rowValues[j]);
+    }
+}
+
+// Завъртане на матрицата на 90 градуса
+int[,] rotatedMatrix = new int[size, size];
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        rotatedMatrix[j, size - i - 1] = matrix[i, j];
+    }
+}
+
+// Отпечатване на завъртената матрица
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        Console.Write(rotatedMatrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
