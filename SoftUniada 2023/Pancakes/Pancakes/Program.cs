741696b9529c82f66e7092770d65b8d045f48363
Nikolay Sinorov <nsinorov123@gmail.com>
2023-02-26 17:47:55 +0200

    Add files via upload

---
+++ SoftUniada 2023/Pancakes/Pancakes/Program.cs
@@ -0,0 +1,29 @@
+﻿string[] input = Console.ReadLine().Split(' ');
+int[] sequence = Array.ConvertAll(input, int.Parse);
+
+int maxSum = int.MinValue;
+int start = 0;
+int end = 0;
+
+int currentSum = 0;
+int currentStart = 0;
+
+for (int i = 0; i < sequence.Length; i++)
+{
+    currentSum += sequence[i];
+
+    if (currentSum > maxSum || (currentSum == maxSum && i - currentStart > end - start))
+    {
+        maxSum = currentSum;
+        start = currentStart;
+        end = i;
+    }
+
+    if (currentSum < 0)
+    {
+        currentSum = 0;
+        currentStart = i + 1;
+    }
+}
+
+Console.WriteLine($"{maxSum} {start} {end}");
